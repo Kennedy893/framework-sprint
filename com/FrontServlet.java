aa58fffc9f212d93f69857799fb3e203138658c7
Kennedy893 <arivelokennedy9@gmail.com>
2025-10-18 14:57:03 +0300

    interception resources

--- com/FrontServlet.java
+++ com/FrontServlet.java
@@ -2,18 +2,51 @@ package com;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class FrontServlet extends HttpServlet 
 {
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException 
     {
-        String url = req.getRequestURI();
-        resp.getWriter().println("Requete URL : " + url);
+        service(req, resp);
     }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException 
+    {
+        String uri = req.getRequestURI();
+        String context = req.getContextPath();
+        String path = getServletContext().getRealPath(uri.substring(context.length()));
+
+        File file = new File(path);
+        resp.setContentType("text/html");
+
+        if (file.exists() && file.isFile()) 
+        {
+            // Vérifie si c’est un fichier statique (html, jsp, etc.)
+            if (uri.endsWith(".html") || uri.endsWith(".jsp")) 
+            {
+                try (FileInputStream fis = new FileInputStream(file);
+                     OutputStream os = resp.getOutputStream()) {
+                    fis.transferTo(os);
+                }
+            } 
+            else {
+                // Si c’est une ressource dynamique
+                RequestDispatcher dispatcher = req.getRequestDispatcher(uri.substring(context.length()));
+                dispatcher.forward(req, resp);
+            }
+        } 
+        else {
+            resp.getWriter().println("<h3>Requete URL : " + uri + "</h3>");
+        }
+    }
+
+
 }
 
-// javac -cp "C:\apache-tomcat-10.1.28\lib\servlet-api.jar" -d build\classes com\FrontServlet.java
-// jar -cvf test_framework.jar -C build\classes  .
-// copy test_framework.jar "..\test-framework\lib"
