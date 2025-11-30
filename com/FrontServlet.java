41e8e19465582a5f4aaec84692bea9aaf0eca6d8
Kennedy893 <arivelokennedy9@gmail.com>
2025-11-30 12:26:33 +0300

    prise en compte de GET et POST

--- com/FrontServlet.java
+++ com/FrontServlet.java
@@ -100,106 +100,47 @@ public class FrontServlet extends HttpServlet
         return args;
     }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException 
+    {
+        handleRequest(req, resp);
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException 
+    {
+        handleRequest(req, resp);
+    }
+
+    protected void handleRequest(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException 
     {
         String uri = req.getRequestURI();
         String context = req.getContextPath();
         String relativePath = uri.substring(context.length());
+        String methodHTTP = req.getMethod().toUpperCase();
 
         // --- CAS 1 : FICHIER STATIC (HTML / JSP) ---------------------------------
         if (handleStaticFile(relativePath, req, resp)) {
             return;
         }
 
-
-        // --- CAS 2 : URL MAPPeE PAR ANNOTATION (CONTROLLER SCANNER) --------------
+        // --- CAS 2 : URL CONTROLLER SCANNER --------------------------------------
         String path = relativePath.isEmpty() ? "/" : relativePath;
 
+        // Récupérer les URL definies pour la methode HTTP correspondante
+        Map<String, UrlDefinition> defs =
+                methodHTTP.equals("POST") ?
+                        ControllerScanner.getPostmappings() :
+                        ControllerScanner.getGetmappings();
 
-        // SPRINT 6 et 6 BIS --------------------------------------------
-        // Map<String, Method> mappings = ControllerScanner.getUrlMappings();
-        // Method method = mappings.get(path);
-
-        // if (method != null) 
-        // {
-        //     try {
-        //         Object controller = method.getDeclaringClass().getDeclaredConstructor().newInstance();
-        //         // Object result = method.invoke(controller);
-
-        //         String controllerName = method.getDeclaringClass().getSimpleName();
-        //         String methodName = method.getName();
-
-        //         //  Injection automatique des paramètres (Méthode 1)
-        //         Object[] args = bindParameters(req, method);
-
-        //         //  Appel de la méthode avec les arguments injectés
-        //         Object result = method.invoke(controller, args);
-
-        //         // --- Si le resultat est un texte simple ---------------------------
-        //         if (result instanceof String) 
-        //         {
-        //             resp.setContentType("text/plain;charset=UTF-8");
-        //             resp.getWriter().println("Resultat retourne : " + result);
-        //         }
-
-        //         // --- Si le resultat est un ModelView ------------------------------
-        //         else if (result instanceof ModelView) 
-        //         {
-        //             ModelView mv = (ModelView) result;
-        //             // String view = mv.getView();
-
-        //             // // Injecte les donnees dans la requete
-        //             // for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
-        //             //     req.setAttribute(entry.getKey(), entry.getValue());
-        //             // }
-
-        //             // RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + view);
-        //             // dispatcher.forward(req, resp);
-        //             // return;
-
-        //             // -Sprint 6 - Injecte les données dans la requête
-        //             for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
-        //                 req.setAttribute(entry.getKey(), entry.getValue());
-        //             }
-
-        //             RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + mv.getView());
-        //             dispatcher.forward(req, resp);
-        //             return;
-        //         }
-
-        //         // --- Affichage --------------------------------------------------------
-        //         resp.setContentType("text/plain;charset=UTF-8");
-        //         resp.getWriter().println("\n URL trouvee : " + path);
-        //         resp.getWriter().println("-> Controleur : " + controllerName);
-        //         resp.getWriter().println("-> Methode : " + methodName);
-
-        //         return;
-
-        //     } catch (Exception e) {
-        //         e.printStackTrace();
-        //         resp.sendError(500, "Erreur interne du serveur : " + e.getMessage());
-        //         return;
-        //     }
-        // }
-
-        // // --- CAS 3 : Aucune URL trouvee 
-        // resp.setContentType("text/plain;charset=UTF-8");
-        // resp.getWriter().println("404 - Aucune methode trouvee pour " + path);
-
-        // -------------------------------------------------------------------
-
-
-        // SPRINT 6 TER ---------------------
-
-        Map<String, UrlDefinition> defs = ControllerScanner.getUrlMappings();
         boolean matched = false;
 
         for (UrlDefinition def : defs.values()) 
         {
-
-            String regex = "^" + def.getRegex() + "$";
+            String regex = "^" + def.getRegex() + "$";  // maka anle {}
 
             if (path.matches(regex)) 
             {
@@ -211,7 +152,7 @@ public class FrontServlet extends HttpServlet
                     String controllerName = method.getDeclaringClass().getSimpleName();
                     String methodName = method.getName();
 
-                    // --- Extraire valeurs dynamiques {id} ---
+                    // --- EXTRACTION {id} / {name} et on la stocke dans un Map --------------------------------
                     Map<String, String> extracted = new HashMap<>();
 
                     String[] pathParts = path.split("/");
@@ -219,74 +160,275 @@ public class FrontServlet extends HttpServlet
 
                     for (int i = 0; i < patternParts.length; i++) 
                     {
-                        if (patternParts[i].startsWith("{")) {
+                        if (patternParts[i].startsWith("{") && patternParts[i].endsWith("}")) 
+                        {
                             String var = patternParts[i].substring(1, patternParts[i].length() - 1);
                             extracted.put(var, pathParts[i]);
                         }
                     }
 
-                    // --- binder les paramètres ---
-
+                    // --- BINDING DES PARAMETRES ---------------------------------
                     Parameter[] params = method.getParameters();
                     Object[] args = new Object[params.length];
 
                     for (int i = 0; i < params.length; i++) 
                     {
-                        String name = params[i].getName();
-                        String strValue = extracted.get(name);
+                        String paramName = params[i].getName();
+                        String strValue = null;
 
+                        // Priorité 1 : URL dynamique {id}
+                        if (extracted.containsKey(paramName)) {
+                            strValue = extracted.get(paramName);
+                        }
+                        // Priorité 2 : POST / GET form-data
+                        else if (req.getParameter(paramName) != null) {
+                            strValue = req.getParameter(paramName);
+                        }
+
+                        // Conversion automatique
                         if (params[i].getType() == int.class || params[i].getType() == Integer.class) {
                             args[i] = Integer.parseInt(strValue);
-                        } 
+                        }
+                        else if (params[i].getType() == double.class || params[i].getType() == Double.class) {
+                            args[i] = Double.parseDouble(strValue);
+                        }
                         else {
-                            args[i] = strValue; // String par défaut
+                            args[i] = strValue;
                         }
                     }
 
-                    // --- Appeler la méthode ---
+                    // --- INVOCATION ------------------------------------------------
                     Object result = method.invoke(controller, args);
 
-                    // --- Gérer le retour ---
+                    // --- GESTION DES RESULTATS -----------------------------------
                     if (result instanceof String) {
                         resp.setContentType("text/plain;charset=UTF-8");
                         resp.getWriter().println(result);
                         return;
                     }
 
-                    if (result instanceof ModelView) {
-                        ModelView mv = (ModelView) result;
-
+                    if (result instanceof ModelView mv) {
                         for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
                             req.setAttribute(entry.getKey(), entry.getValue());
                         }
-
                         RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + mv.getView());
                         dispatcher.forward(req, resp);
                         return;
                     }
 
-                    // --- Affichage --------------------------------------------------------
+                    // AFFICHAGE DÉBUG ---------------------------------------------
                     resp.setContentType("text/plain;charset=UTF-8");
-                    resp.getWriter().println("\n URL trouvee : " + path);
-                    resp.getWriter().println("-> Controleur : " + controllerName);
-                    resp.getWriter().println("-> Methode : " + methodName);
-                    
+                    resp.getWriter().println("\n URL trouvée : " + path);
+                    resp.getWriter().println("-> Méthode HTTP : " + methodHTTP);
+                    resp.getWriter().println("-> Contrôleur : " + controllerName);
+                    resp.getWriter().println("-> Méthode : " + methodName);
+                    return;
+
                 } catch (Exception e) {
                     e.printStackTrace();
                     resp.sendError(500, "Erreur URL dynamique : " + e.getMessage());
                     return;
                 }
-
             }
         }
 
-        // Si aucun motif ne correspond
+        // --- AUCUNE URL TROUVÉE --------------------------------------------------
         if (!matched) {
             resp.setContentType("text/plain;charset=UTF-8");
             resp.getWriter().println("404 - Aucune méthode trouvée pour " + path);
         }
-
     }
+
+
+
+
+    // @Override
+    // protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException 
+    // {
+    //     String uri = req.getRequestURI();
+    //     String context = req.getContextPath();
+    //     String relativePath = uri.substring(context.length());
+
+    //     // --- CAS 1 : FICHIER STATIC (HTML / JSP) ---------------------------------
+    //     if (handleStaticFile(relativePath, req, resp)) {
+    //         return;
+    //     }
+
+
+    //     // --- CAS 2 : URL MAPPeE PAR ANNOTATION (CONTROLLER SCANNER) --------------
+    //     String path = relativePath.isEmpty() ? "/" : relativePath;
+
+
+    //     // SPRINT 6 et 6 BIS --------------------------------------------
+    //     // Map<String, Method> mappings = ControllerScanner.getUrlMappings();
+    //     // Method method = mappings.get(path);
+
+    //     // if (method != null) 
+    //     // {
+    //     //     try {
+    //     //         Object controller = method.getDeclaringClass().getDeclaredConstructor().newInstance();
+    //     //         // Object result = method.invoke(controller);
+
+    //     //         String controllerName = method.getDeclaringClass().getSimpleName();
+    //     //         String methodName = method.getName();
+
+    //     //         //  Injection automatique des paramètres (Méthode 1)
+    //     //         Object[] args = bindParameters(req, method);
+
+    //     //         //  Appel de la méthode avec les arguments injectés
+    //     //         Object result = method.invoke(controller, args);
+
+    //     //         // --- Si le resultat est un texte simple ---------------------------
+    //     //         if (result instanceof String) 
+    //     //         {
+    //     //             resp.setContentType("text/plain;charset=UTF-8");
+    //     //             resp.getWriter().println("Resultat retourne : " + result);
+    //     //         }
+
+    //     //         // --- Si le resultat est un ModelView ------------------------------
+    //     //         else if (result instanceof ModelView) 
+    //     //         {
+    //     //             ModelView mv = (ModelView) result;
+    //     //             // String view = mv.getView();
+
+    //     //             // // Injecte les donnees dans la requete
+    //     //             // for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
+    //     //             //     req.setAttribute(entry.getKey(), entry.getValue());
+    //     //             // }
+
+    //     //             // RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + view);
+    //     //             // dispatcher.forward(req, resp);
+    //     //             // return;
+
+    //     //             // -Sprint 6 - Injecte les données dans la requête
+    //     //             for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
+    //     //                 req.setAttribute(entry.getKey(), entry.getValue());
+    //     //             }
+
+    //     //             RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + mv.getView());
+    //     //             dispatcher.forward(req, resp);
+    //     //             return;
+    //     //         }
+
+    //     //         // --- Affichage --------------------------------------------------------
+    //     //         resp.setContentType("text/plain;charset=UTF-8");
+    //     //         resp.getWriter().println("\n URL trouvee : " + path);
+    //     //         resp.getWriter().println("-> Controleur : " + controllerName);
+    //     //         resp.getWriter().println("-> Methode : " + methodName);
+
+    //     //         return;
+
+    //     //     } catch (Exception e) {
+    //     //         e.printStackTrace();
+    //     //         resp.sendError(500, "Erreur interne du serveur : " + e.getMessage());
+    //     //         return;
+    //     //     }
+    //     // }
+
+    //     // // --- CAS 3 : Aucune URL trouvee 
+    //     // resp.setContentType("text/plain;charset=UTF-8");
+    //     // resp.getWriter().println("404 - Aucune methode trouvee pour " + path);
+
+    //     // -------------------------------------------------------------------
+
+
+    //     // SPRINT 6 TER ---------------------
+
+    //     Map<String, UrlDefinition> defs = ControllerScanner.getUrlMappings();
+    //     boolean matched = false;
+
+    //     for (UrlDefinition def : defs.values()) 
+    //     {
+
+    //         String regex = "^" + def.getRegex() + "$";
+
+    //         if (path.matches(regex)) 
+    //         {
+    //             matched = true;
+
+    //             try {
+    //                 Method method = def.getMethod();
+    //                 Object controller = method.getDeclaringClass().getDeclaredConstructor().newInstance();
+    //                 String controllerName = method.getDeclaringClass().getSimpleName();
+    //                 String methodName = method.getName();
+
+    //                 // --- Extraire valeurs dynamiques {id} ---
+    //                 Map<String, String> extracted = new HashMap<>();
+
+    //                 String[] pathParts = path.split("/");
+    //                 String[] patternParts = def.getPattern().split("/");
+
+    //                 for (int i = 0; i < patternParts.length; i++) 
+    //                 {
+    //                     if (patternParts[i].startsWith("{")) {
+    //                         String var = patternParts[i].substring(1, patternParts[i].length() - 1);
+    //                         extracted.put(var, pathParts[i]);
+    //                     }
+    //                 }
+
+    //                 // --- binder les paramètres ---
+
+    //                 Parameter[] params = method.getParameters();
+    //                 Object[] args = new Object[params.length];
+
+    //                 for (int i = 0; i < params.length; i++) 
+    //                 {
+    //                     String name = params[i].getName();
+    //                     String strValue = extracted.get(name);
+
+    //                     if (params[i].getType() == int.class || params[i].getType() == Integer.class) {
+    //                         args[i] = Integer.parseInt(strValue);
+    //                     } 
+    //                     else {
+    //                         args[i] = strValue; // String par défaut
+    //                     }
+    //                 }
+
+    //                 // --- Appeler la méthode ---
+    //                 Object result = method.invoke(controller, args);
+
+    //                 // --- Gérer le retour ---
+    //                 if (result instanceof String) {
+    //                     resp.setContentType("text/plain;charset=UTF-8");
+    //                     resp.getWriter().println(result);
+    //                     return;
+    //                 }
+
+    //                 if (result instanceof ModelView) {
+    //                     ModelView mv = (ModelView) result;
+
+    //                     for (Map.Entry<String, Object> entry : mv.getData().entrySet()) {
+    //                         req.setAttribute(entry.getKey(), entry.getValue());
+    //                     }
+
+    //                     RequestDispatcher dispatcher = req.getRequestDispatcher("/views/" + mv.getView());
+    //                     dispatcher.forward(req, resp);
+    //                     return;
+    //                 }
+
+    //                 // --- Affichage --------------------------------------------------------
+    //                 resp.setContentType("text/plain;charset=UTF-8");
+    //                 resp.getWriter().println("\n URL trouvee : " + path);
+    //                 resp.getWriter().println("-> Controleur : " + controllerName);
+    //                 resp.getWriter().println("-> Methode : " + methodName);
+                    
+    //             } catch (Exception e) {
+    //                 e.printStackTrace();
+    //                 resp.sendError(500, "Erreur URL dynamique : " + e.getMessage());
+    //                 return;
+    //             }
+
+    //         }
+    //     }
+
+    //     // Si aucun motif ne correspond
+    //     if (!matched) {
+    //         resp.setContentType("text/plain;charset=UTF-8");
+    //         resp.getWriter().println("404 - Aucune méthode trouvée pour " + path);
+    //     }
+
+    // }
+
 
 
 
--- init/UrlDefinition.java
+++ init/UrlDefinition.java
@@ -3,21 +3,33 @@ package init;
 import java.lang.reflect.Method;
 import java.util.List;
 
-public class UrlDefinition {
-    private String pattern;     // ex: /etudiant/delete/{id}
-    private String regex;       // ex: /etudiant/delete/([^/]+)
-    private List<String> vars;  // ex: ["id"]
-    private Method method;
+public class UrlDefinition 
+{
 
-    public UrlDefinition(String pattern, String regex, List<String> vars, Method method) {
+    private String pattern;
+    private String regex;
+    private List<String> variables;
+    private Method method;
+    private String httpMethod; // "GET" ou "POST"
+
+    public UrlDefinition(String pattern, String regex, List<String> vars, Method method, String httpMethod) {
         this.pattern = pattern;
         this.regex = regex;
-        this.vars = vars;
+        this.variables = vars;
         this.method = method;
+        this.httpMethod = httpMethod;
     }
 
     public String getPattern() { return pattern; }
     public String getRegex() { return regex; }
-    public List<String> getVars() { return vars; }
+    public List<String> getVariables() { return variables; }
     public Method getMethod() { return method; }
+    public String getHttpMethod() { return httpMethod; }
 }
+
+
+// "/user/{id}",          // pattern
+// "/user/([^/]+)",       // regex pour matcher le {id}
+//  vars,                 // liste des variables
+//  getUserMethod,        // méthode Java à appeler
+//  "GET"                 // type HTTP
